038c42667df42d5203eef5342d8a3cead83f680c
Xuandat1809 <91858880+Xuandat1809@users.noreply.github.com>
2022-08-06 13:43:33 +0700

    Add files via upload

---
+++ Bai5.cpp
@@ -0,0 +1,18 @@
+Bài 5:
+Cho cây T trong đó V={1, 2, ..., n} là tập đỉnh và E là tập cạnh. Lập trình tính
+số Q các cách phân hoạch tập cạnh E thành 2 tập con E1 và E2 sao cho các cạnh 
+thuộc E1 lập thành 1 cây con của T và các cạnh của E2 cũng lập thành 1 cây con của T
+iNPUT:
+- dÒNG 1: GHI SỐ NGUYÊN DUONG n (2 <= n <= 100000)
+-dòng i+1:(i = 1,2,...,n-1): ghi 2 số nguyên dương u và v là 2 đầu mút của cạnh thứ i trong E
+Output:
+Ghi ra giá trị Q modulo 10000
+Ví dụ:
+Input
+5
+2 4
+4 5
+2 1
+5 3
+Output:
+3
