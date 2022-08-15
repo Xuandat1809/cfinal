8ffef7f38300b8263dd960f758d14fb84f73097f
Xuandat1809 <91858880+Xuandat1809@users.noreply.github.com>
2022-08-15 20:24:50 +0700

    Delete chatfile.cpp

--- chatfile.cpp
+++
@@ -1,6 +0,0 @@
-a ơi kiểm tra lại giúp e bài 1234 với sai testcase gần hết
-với cả còn bài 5 nữa ạ
-anh chưa gửi em bài 3 đâu 
-anh mới gửi em 3 bài 1,2,4 tưởng đương cái ý trong đề
-em kiểm tra xem nộp đúng chưa
-trường hợp bị sai em đổi hết [30] -> [50] ở kiểu dữ liệu dạng char xem nhé.
